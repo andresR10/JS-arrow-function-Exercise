e672a5dcd5253bed73b568e4d1dd3db3b8a275d5
AndresR <andres.rodag1@gmail.com>
2023-04-16 12:44:21 -0400

    completed arrow function exercise

---
+++ app.js
@@ -0,0 +1,23 @@
+// ES5 Map Callback 
+function double(arr){
+    return arr.map(function(val){
+        return val * 2;
+    });
+};
+// above turned into ES2015 shorthand 
+const double => arr => arr.map(val => val * 2);
+
+
+// ES5
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+        return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+        return square % 2 === 0;
+    });
+    return evens;
+}
+//ES2015
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
+
